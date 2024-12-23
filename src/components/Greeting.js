bc321df10f6dab115f8ffe125539efdee0c104e1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-23 14:57:42 +0200

    Update Greeting.js

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
   const [changedText, setChangedText] = useState(false);
@@ -10,8 +11,10 @@ const Greeting = () => {
   return (
     <div>
       <h2>Hello World</h2>
-      {!changedText && <p>it's good to see you</p>}
-      {changedText && <p>Changed</p>}
+      {/* {!changedText && <p>it's good to see you</p>} */}
+      {!changedText && <Output>it's good to see you</Output>}
+      {/* {changedText && <p>Changed</p>} */}
+      {changedText && <Output>Changed</Output>}
       <button onClick={changeText}>Change text</button>
     </div>
   );
